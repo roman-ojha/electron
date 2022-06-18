214a1656a02adca199ade4a31fb397641677e6aa
Roman-Ojha <razzroman98@gmail.com>
2022-06-18 10:40:55 +0545

    docs(tutorial): frame less app

---
+++ Class/src/09_Frame_less_App/main.js
@@ -0,0 +1,45 @@
+const electron = require("electron");
+const app = electron.app;
+
+app.disableHardwareAcceleration();
+
+const BrowserWindow = electron.BrowserWindow;
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    title: "Electron",
+    // to disable frame
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  win.loadFile("index.html");
+
+  win.webContents.openDevTools();
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  // when all the window get closed
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
