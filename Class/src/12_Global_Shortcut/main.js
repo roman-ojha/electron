eb8a073a4ae479e697b07a554faf75e3438fac0e
Roman-Ojha <razzroman98@gmail.com>
2022-06-18 11:39:19 +0545

    docs(tutorial): Global Shortcut

---
+++ Class/src/12_Global_Shortcut/main.js
@@ -0,0 +1,43 @@
+const { BrowserWindow, app, globalShortcut } = require("electron");
+app.disableHardwareAcceleration();
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    title: "Electron",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  win.loadFile("index.html");
+  globalShortcut.register("Shift+K", () => {
+    // this function will get called when we will press 'K'
+    win.loadFile("other.html");
+    console.log("Pressed K");
+  });
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  // we can also use it after it get ready
+  globalShortcut.register("Shift+A", () => {
+    console.log("Pressed A");
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
