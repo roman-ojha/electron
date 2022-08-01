206e1c19a8826932dc96ea7a956da6861e68d36c
Roman-Ojha <razzroman98@gmail.com>
2022-08-01 23:06:47 +0545

    feat: runned and build react-electron successfully

---
+++ Tutorial/02_react_electron/public/main.js
@@ -0,0 +1,45 @@
+// import { BrowserWindow, app } from "electron";
+// import path from "path";
+const { BrowserWindow, app } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      //   nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+
+  require("@electron/remote/main").initialize();
+  require("@electron/remote/main").enable(win.webContents);
+
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : //   if we are in dev run electron on localhost 3000
+        `file://${path.join(__dirname, "../build/index.html")}`
+    // else if we are in production for building application then we will run build version of react
+  );
+}
+
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
--- Tutorial/02_react_electron/src/index.js
+++ Tutorial/02_react_electron/src/index.js
@@ -1,3 +1,29 @@
+/*
+  *> Setup:
+    -> npx create-react-app <project_name>
+    -> yarn add -D electron concurrently wait-on cross-env
+    -> yarn add @electron/remote (to handle IPC)
+    -> in package.json:
+      "scripts": {
+        "electron:serve": "concurrently -k \"cross-env BROWSER=none yarn start\" \"yarn electron:start\"",
+        // cross-env will help us to not open react app on browser and then we will run 'electron:start'
+        "electron:build": "yarn build && electron-builder -c.extraMetadata.main=build/main.js",
+        // firstly we will build a react application after that we will build electron app
+        // we also have to set the set the main entry point of the electron app (main.js) to point to build directory instead of a public/main.js folder
+        "electron:start": "wait-on tcp:3000 && electron ."
+        // wait-on package will help to wait until react start running on port 3000 & then start electron
+      },
+    -> after when electron run it will look into the package.json file for javascript entry point for that we will add this on package.json
+      -> "main": "src/main/main.js",
+        "homepage": "./",
+    -> yarn electron:serve (to start electron with react)
+  
+  *) Build App:
+      -> yarn add -D electron-builder
+      -> yarn add electron-is-dev
+      -> yarn electron:build (for windows)
+*/
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
